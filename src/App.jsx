ea004240a4e91bb599c762f7a52ebbc1eeb63712
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-02 21:11:08 +0100

    Create empty files for Landing page & PersonalInformation component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styles from './App.module.css';
+
+const App = () => {
+    return (
+        <div></div>
+    )
+};
+
+export default App;
