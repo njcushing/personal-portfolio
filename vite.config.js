c98b55534f8d59804b6316998b3d05d7cbeacaa2
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-02 20:53:59 +0100

    Remove Vite & React boilerplate and setup file structure

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        // Makes @ resolve to src for more readable import filepaths
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: "./testSetup.js",
+        setupFiles: "./src/testSetup.js",
     },
 });
