c9fdebd1bf3de3a2c752aa1208003d38a7ee5ac3
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-03 19:04:17 +0100

    Add h4 element containing some 'about me' lipsum sample text

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -6,6 +6,14 @@ const PersonalInformation = () => {
     return (
         <>
             <h2 className={styles["name"]}>Niall Cushing</h2>
+            <h4 className={styles["about-me"]}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                gravida lorem magna. Praesent ut tincidunt lectus. Cras ut
+                fermentum orci. Nulla auctor dui eros, vitae imperdiet justo
+                dictum ut. Fusce blandit risus et elit convallis, id finibus
+                nulla porta. Integer tempor nisl et purus aliquam, eu faucibus
+                urna sagittis. Etiam.
+            </h4>
         </>
     )
 };
