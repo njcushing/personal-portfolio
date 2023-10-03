084b553dd03de00ea788791450def5fe1d3ce168
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-03 20:55:17 +0100

    Import more fonts and add new styles to PersonalInformation

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -4,7 +4,7 @@ import photo from '@/assets/NiallCushingPhoto.jpg';
 
 const PersonalInformation = () => {
     return (
-        <div className="container">
+        <div className={styles["container"]}>
             <img
                 className={styles["photo"]}
                 src={photo}
