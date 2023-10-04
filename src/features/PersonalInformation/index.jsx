bc95c6b60178ed10333cbdcbe133de3f80ec4a80
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-04 22:26:08 +0100

    Add LinkedIn and GitHub symbols with basic styling

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -1,6 +1,13 @@
 import styles from './index.module.css';
 
 import photo from '@/assets/NiallCushingPhoto.jpg';
+import MaterialSymbolsButton from './components/MaterialSymbolsButton/MaterialSymbolsButton';
+
+const materialSymbolsStyle = {
+    color: 'black',
+    fontSize: '2rem',
+
+}
 
 const PersonalInformation = () => {
     return (
@@ -13,14 +20,34 @@ const PersonalInformation = () => {
             <h2 className={styles["name"]}>Niall Cushing</h2>
             <div className={styles["about-me-container"]}>
                 <h3 className={styles["about-me-title"]}>About Me</h3>
-                <h4 className={styles["about-me-text"]}>
+                <p className={styles["about-me-text"]}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                     gravida lorem magna. Praesent ut tincidunt lectus. Cras ut
                     fermentum orci. Nulla auctor dui eros, vitae imperdiet justo
                     dictum ut. Fusce blandit risus et elit convallis, id finibus
                     nulla porta. Integer tempor nisl et purus aliquam, eu
                     faucibus urna sagittis. Etiam.
-                </h4>
+                </p>
+            </div>
+            <div className={styles["contact-me-container"]}>
+                <h3 className={styles["contact-me-title"]}>Contact Me</h3>
+                <ul className={styles["contact-me-options"]}>
+                    <li className={styles["contact-me-button"]}>
+                        <img
+                            className={styles["linked-in-icon"]}
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+                            aria-label="linked-in"
+                        />
+                    </li>
+                    <li className={styles["contact-me-button"]}>
+                        <img
+                            className={styles["github-icon"]}
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                            aria-label="github"
+                        />
+                    </li>
+                    <li className={styles["contact-me-button"]}><MaterialSymbolsButton text="email" link="/"/></li>
+                </ul>
             </div>
         </div>
     )
