6d957b24d99e51da9cda5a6723a1f7d9c9321f9b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-05 10:35:23 +0100

    Add more paragraphs to 'About Me' section

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -15,14 +15,20 @@ const PersonalInformation = () => {
             <h2 className={styles["name"]}>Niall Cushing</h2>
             <div className={styles["about-me-container"]}>
                 <h3 className={styles["about-me-title"]}>About Me</h3>
-                <p className={styles["about-me-text"]}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    gravida lorem magna. Praesent ut tincidunt lectus. Cras ut
-                    fermentum orci. Nulla auctor dui eros, vitae imperdiet justo
-                    dictum ut. Fusce blandit risus et elit convallis, id finibus
-                    nulla porta. Integer tempor nisl et purus aliquam, eu
-                    faucibus urna sagittis. Etiam.
-                </p>
+                <ul className={styles["about-me-paragraph-list"]}>
+                    <li><p className={styles["about-me-paragraph"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                        gravida lorem magna. Praesent ut tincidunt lectus.
+                    </p></li>
+                    <li><p className={styles["about-me-paragraph"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                        gravida lorem magna. Praesent ut tincidunt lectus.
+                    </p></li>
+                    <li><p className={styles["about-me-paragraph"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                        gravida lorem magna. Praesent ut tincidunt lectus.
+                    </p></li>
+                </ul>
             </div>
             <div className={styles["contact-me-container"]}>
                 <h3 className={styles["contact-me-title"]}>Contact Me</h3>
