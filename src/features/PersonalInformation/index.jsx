02e8fd2a1f3317a9593d4bd4c08cf7d379061825
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-05 12:17:28 +0100

    Add media query at 600px to alter layout of info panel

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -32,40 +32,42 @@ const PersonalInformation = () => {
             </div>
             <div className={styles["contact-me-container"]}>
                 <h3 className={styles["contact-me-title"]}>Contact Me</h3>
-                <ul className={styles["contact-me-links"]}>
-                    <li><DeviconsAnchor
-                        href="https://uk.linkedin.com/in/niall-cushing-19b8ab17a"
-                        ariaLabel="linked in"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
-                        alt="linked in"
-                        sizeRem={2.4}
-                    /></li>
-                    <li><DeviconsAnchor
-                        href="https://github.com/njcushing"
-                        ariaLabel="github"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                        alt="github"
-                        sizeRem={2.4}
-                    /></li>
-                </ul>
-                <ul className={styles["contact-me-additional"]}>
-                    <li>
-                        <h4 className={styles["contact-me-additional-item-heading"]}>
-                            Email:
-                        </h4>
-                        <p className={styles["contact-me-additional-item-text"]}>
-                            fakeemail@gmail.com
-                        </p>
-                    </li>
-                    <li>
-                        <h4 className={styles["contact-me-additional-item-heading"]}>
-                            Phone:
-                        </h4>
-                        <p className={styles["contact-me-additional-item-text"]}>
-                            +44 9999 999999
-                        </p>
-                    </li>
-                </ul>
+                <div className={styles["contact-information-container"]}>
+                    <ul className={styles["contact-me-links"]}>
+                        <li><DeviconsAnchor
+                            href="https://uk.linkedin.com/in/niall-cushing-19b8ab17a"
+                            ariaLabel="linked in"
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+                            alt="linked in"
+                            sizeRem={2.4}
+                        /></li>
+                        <li><DeviconsAnchor
+                            href="https://github.com/njcushing"
+                            ariaLabel="github"
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                            alt="github"
+                            sizeRem={2.4}
+                        /></li>
+                    </ul>
+                    <ul className={styles["contact-me-additional"]}>
+                        <li>
+                            <h4 className={styles["contact-me-additional-item-heading"]}>
+                                Email:
+                            </h4>
+                            <p className={styles["contact-me-additional-item-text"]}>
+                                fakeemail@gmail.com
+                            </p>
+                        </li>
+                        <li>
+                            <h4 className={styles["contact-me-additional-item-heading"]}>
+                                Phone:
+                            </h4>
+                            <p className={styles["contact-me-additional-item-text"]}>
+                                +44 9999 999999
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
