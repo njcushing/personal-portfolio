49b931c5601c6f3f3475ba0e20ebc6821e183425
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-05 10:54:30 +0100

    Write test suite for component

---
+++ src/features/PersonalInformation/components/MaterialSymbolsAnchor/MaterialSymbolsAnchor.test.jsx
@@ -0,0 +1,47 @@
+import { vi } from 'vitest'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import MaterialSymbolsAnchor from './MaterialSymbolsAnchor'
+
+describe("UI/DOM Testing...", () => {
+    describe("The anchor element...", () => {
+        test(`Should have an accessible name (aria-label) attribute whose value
+         is the same as the provided 'ariaLabel' prop's value`, () => {
+            render(
+                <MaterialSymbolsAnchor
+                    href="https://www.a.com/"
+                    ariaLabel="test"
+                    text="test text"
+                />
+            );
+            const anchor = screen.getByRole("link", { name: /test/i });
+            expect(anchor).toBeInTheDocument;
+        });
+        test(`Should have an href attribute whose value is the same as the
+         provided 'href' prop's value`, () => {
+            render(
+                <MaterialSymbolsAnchor
+                    href="https://www.a.com/"
+                    ariaLabel="test"
+                    text="test text"
+                />
+            );
+            const anchor = screen.getByRole("link", { name: /test/i });
+            expect(anchor.href).toBe("https://www.a.com/");
+        });
+    });
+    describe("The div element containing the text...", () => {
+        test(`Should have textContent equal to the provided 'text' prop's value`, () => {
+            render(
+                <MaterialSymbolsAnchor
+                    href="https://www.a.com/"
+                    ariaLabel="test"
+                    text="test text"
+                />
+            );
+            const anchor = screen.getByRole("listitem");
+            expect(anchor.textContent).toBe("test text");
+        });
+    });
+});
