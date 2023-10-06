4cc967af155389e58f178b359d7719a12e7a3585
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-06 11:59:28 +0100

    Add ProjectList title and category title for TOP projects

--- src/features/ProjectList/index.jsx
+++ src/features/ProjectList/index.jsx
@@ -1,9 +1,13 @@
 import styles from './index.module.css';
 
+import ProjectPanel from './components/ProjectPanel/ProjectPanel';
+
 const ProjectList = () => {
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
+            <h2 className={styles["title"]}>My Projects</h2>
+            <h3 className={styles["project-category-title"]}>The Odin Project</h3>
         </div>
         </div>
     );
