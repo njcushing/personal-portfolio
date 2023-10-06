713f232a08105355296ba7a9830abf7966154399
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-06 11:58:57 +0100

    Create new boilerplate directory for ProjectPanel component

---
+++ src/features/ProjectList/components/ProjectPanel/ProjectPanel.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectPanel.module.css';
+
+const ProjectPanel = () => {
+    return (
+        <div className={styles["wrapper"]}>
+
+        </div>
+    );
+};
+
+export default ProjectPanel;
