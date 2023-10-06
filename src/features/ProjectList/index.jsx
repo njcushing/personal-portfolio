65374279ee0c4ee51d8beab07f9f11297f8fa83f
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-06 11:46:45 +0100

    Create new boilerplate directory for ProjectList feature

---
+++ src/features/ProjectList/index.jsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+const ProjectList = () => {
+    return (
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+        </div>
+        </div>
+    );
+};
+
+export default ProjectList;
