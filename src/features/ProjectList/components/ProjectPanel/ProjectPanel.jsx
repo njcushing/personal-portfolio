e5864b664c5fe9093e28cb1d387efc599fc64a9b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-07 09:43:45 +0100

    Add placeholder elements for project image and name

--- src/features/ProjectList/components/ProjectPanel/ProjectPanel.jsx
+++ src/features/ProjectList/components/ProjectPanel/ProjectPanel.jsx
@@ -1,12 +1,36 @@
+import PropTypes from 'prop-types';
 import styles from './ProjectPanel.module.css';
 
-const ProjectPanel = () => {
+const ProjectPanel = ({
+    imgSrc,
+    imgAlt,
+    projectName,
+}) => {
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
+            <img
+                className={styles["image"]}
+                src={imgSrc}
+                alt={imgAlt}
+            ></img>
+            <h4 className={styles["name"]}>{projectName}</h4>
+            <p></p>
         </div>
         </div>
     );
 };
+
+ProjectPanel.propTypes = {
+    imgSrc: PropTypes.string,
+    imgAlt: PropTypes.string,
+    projectName: PropTypes.string,
+}
+
+ProjectPanel.defaultProps = {
+    imgSrc: "",
+    imgAlt: "",
+    projectName: "Project Name",
+}
 
 export default ProjectPanel;
