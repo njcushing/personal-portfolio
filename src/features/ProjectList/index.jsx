6a996f1fdc7f5790912042849bf6339804fb0aa5
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-07 12:56:43 +0100

    Add the start of new logic for ProjectList feature
    I'm trying to create an effect where any given ProjectList can have a
    set maximum number of rows displayed, which reacts to the dynamic
    nature of the grid container for the ProjectPanel components. For
    example, if the default is 2 rows, when the containing element for the
    ProjectList is wide enough that 3 columns can be displayed, this means
    6 ProjectPanels will be displayed. If the user then shrinks the window
    down to 2 columns, only 4 will be displayed. The user should be able to
    click a 'Show More' button to increase the number of rows and therefore
    the number of displayed ProjectPanels in the grid, if any. So far, I
    have achieved making the component re-render when the number of columns
    changes, by memoising and accessing the element's computed styles.

--- src/features/ProjectList/index.jsx
+++ src/features/ProjectList/index.jsx
@@ -1,23 +1,72 @@
+import { useState, useEffect, useRef, useMemo } from 'react';
 import styles from './index.module.css';
 
 import photo from '@/assets/NiallCushingPhoto.jpg';
 import ProjectPanel from './components/ProjectPanel/ProjectPanel';
 
+const findGridColumns = (grid) => {
+    try {
+        const gridComputedStyle = window.getComputedStyle(grid);
+        const columnWidthsString = gridComputedStyle.getPropertyValue("grid-template-columns");
+        const numberOfColumns = columnWidthsString.split(" ").length;
+        return numberOfColumns;
+    } catch(error) {
+        return null;
+    }
+};
+
+const projects = (<>
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+    <ProjectPanel />
+</>);
+
 const ProjectList = () => {
+    const [rowCount, setRowCount] = useState(2);
+    const [columnCount, setColumnCount] = useState(0);
+
+    const projectListRef = useRef(null);
+    const projectList = useMemo(() => {
+        return(
+            <div
+                className={styles["project-category-projects"]}
+                ref={projectListRef}
+            >
+                {projects}
+            </div>
+        );
+    }, []);
+
+    useEffect(() => {
+        const numberOfColumns = findGridColumns(projectListRef.current);
+        if (numberOfColumns === null) return;
+        if (columnCount !== numberOfColumns) setColumnCount(numberOfColumns);
+    }, [projectList, columnCount]);
+
+    useEffect(() => {
+        const resizeEventListener = () => {
+            const numberOfColumns = findGridColumns(projectListRef.current);
+            if (numberOfColumns === null) return;
+            if (columnCount !== numberOfColumns) setColumnCount(numberOfColumns);
+        };
+        window.addEventListener("resize", resizeEventListener);
+        return () => window.removeEventListener("resize", resizeEventListener);
+    }, [columnCount]);
+
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
             <h2 className={styles["title"]}>My Projects</h2>
             <div className={styles["project-category"]}>
                 <h3 className={styles["project-category-title"]}>The Odin Project</h3>
-                <div className={styles["project-category-projects"]}>
-                    <ProjectPanel />
-                    <ProjectPanel />
-                    <ProjectPanel />
-                    <ProjectPanel />
-                    <ProjectPanel />
-                    <ProjectPanel />
-                </div>
+                {projectList}
             </div>
         </div>
         </div>
