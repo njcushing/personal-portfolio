cac1c3a2e371e2f5f0e0e1e171e9276983ac61af
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-08 11:50:50 +0100

    Refactor PersonalInformation component
    I have moved the ContactInformation to its own component and have
    therefore removed it from the 'About Me' section of the page, although
    I have left behind the LinkedIn and GitHub page links.

--- src/features/PersonalInformation/index.jsx
+++ src/features/PersonalInformation/index.jsx
@@ -1,7 +1,6 @@
 import styles from './index.module.css';
 
 import photo from '@/assets/NiallCushingPhoto.jpg';
-import MaterialSymbolsAnchor from '@/components/MaterialSymbolsAnchor/MaterialSymbolsAnchor';
 import DeviconsAnchor from '@/components/DeviconsAnchor/DeviconsAnchor';
 
 const PersonalInformation = () => {
@@ -14,6 +13,22 @@ const PersonalInformation = () => {
                 alt="Me in front of the Prague skyline"
             ></img>
             <h2 className={styles["name"]}>Niall Cushing</h2>
+            <ul className={styles["personal-page-links"]}>
+                <li><DeviconsAnchor
+                    href="https://uk.linkedin.com/in/niall-cushing-19b8ab17a"
+                    ariaLabel="linked in"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+                    alt="linked in"
+                    sizeRem={2.4}
+                /></li>
+                <li><DeviconsAnchor
+                    href="https://github.com/njcushing"
+                    ariaLabel="github"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                    alt="github"
+                    sizeRem={2.4}
+                /></li>
+            </ul>
             <div className={styles["about-me-container"]}>
                 <h3 className={styles["about-me-title"]}>About Me</h3>
                 <ul className={styles["about-me-paragraph-list"]}>
@@ -30,45 +45,6 @@ const PersonalInformation = () => {
                         gravida lorem magna. Praesent ut tincidunt lectus.
                     </p></li>
                 </ul>
-            </div>
-            <div className={styles["contact-me-container"]}>
-                <h3 className={styles["contact-me-title"]}>Contact Me</h3>
-                <div className={styles["contact-information-container"]}>
-                    <ul className={styles["contact-me-links"]}>
-                        <li><DeviconsAnchor
-                            href="https://uk.linkedin.com/in/niall-cushing-19b8ab17a"
-                            ariaLabel="linked in"
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
-                            alt="linked in"
-                            sizeRem={2.4}
-                        /></li>
-                        <li><DeviconsAnchor
-                            href="https://github.com/njcushing"
-                            ariaLabel="github"
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                            alt="github"
-                            sizeRem={2.4}
-                        /></li>
-                    </ul>
-                    <ul className={styles["contact-me-additional"]}>
-                        <li>
-                            <h4 className={styles["contact-me-additional-item-heading"]}>
-                                Email:
-                            </h4>
-                            <p className={styles["contact-me-additional-item-text"]}>
-                                fakeemail@gmail.com
-                            </p>
-                        </li>
-                        <li>
-                            <h4 className={styles["contact-me-additional-item-heading"]}>
-                                Phone:
-                            </h4>
-                            <p className={styles["contact-me-additional-item-text"]}>
-                                +44 9999 999999
-                            </p>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </div>
         </div>
