d5f4ce8e7899ea55d434102bdfa690ce5020f614
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-08 11:23:12 +0100

    Create boilerplate folder structure for new ContactInformation feature

---
+++ src/features/ContactInformation/index.jsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+const ContactInformation = () => {
+    return (
+        <div className={styles["wrapper"]}>
+        <nav className={styles["container"]}>
+        </nav>
+        </div>
+    );
+};
+
+export default ContactInformation;
