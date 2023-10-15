2ba55eb763b952991a18c7e15575b8a5891e3391
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-15 13:21:30 +0100

    Update test suite for ProjectPanel component

--- src/features/Projects/components/ProjectPanel/ProjectPanel.test.jsx
+++ src/features/Projects/components/ProjectPanel/ProjectPanel.test.jsx
@@ -18,6 +18,9 @@ const mockParams = {
     imgSrc: "https://www.a.com/",
     imgAlt: "This is not an image",
     technologies: ["html", "css", "javascript"],
+    desktopCompatible: false,
+    mobileCompatible: false,
+    tabletCompatible: false,
     pageUrl: "https://www.b.com/",
     githubUrl: "https://www.c.com/",
 }
@@ -37,6 +40,13 @@ vi.mock('@/utils/technologiesInformation/technologiesInformation', async () => {
     };
 });
 
+const MaterialSymbolsIconRenderMock = vi.fn(({ text }) => {
+    return <div aria-label="device-compatibility-icon">{text}</div>;
+});
+vi.mock('@/components/MaterialSymbolsIcon/MaterialSymbolsIcon', () => ({ 
+    default: ({ text }) => MaterialSymbolsIconRenderMock({ text }),
+}));
+
 vi.mock('@/components/MaterialSymbolsAnchor/MaterialSymbolsAnchor', () => ({ 
     default: ({ href }) => <a href={href} aria-label="page-link"></a>,
 }));
@@ -49,14 +59,14 @@ describe("UI/DOM Testing...", () => {
     describe("The img element...", () => {
         test(`Should have src and alt attributes equal to the provided 'imgSrc'
          and 'imgAlt' props' values, respectively`, () => {
-            render(<ProjectPanel params={mockParams} />);
+            renderComponent();
             const ele = screen.getByRole("img", { name: /This is not an image/i });
             expect(ele).toBeInTheDocument();
          });
     });
     describe("The header element containing the project's name...", () => {
         test(`Should have textContent equal to the provided 'name' prop's value`, () => {
-            render(<ProjectPanel params={mockParams} />);
+            renderComponent();
             const ele = screen.getByRole("heading", { name: /Test/i });
             expect(ele).toBeInTheDocument();
          });
@@ -65,7 +75,7 @@ describe("UI/DOM Testing...", () => {
         test(`Should contain one paragraph element for each entry in the
          provided 'desc' prop's array, the textContent of which should be equal
          to the string value of that entry in the array`, () => {
-            render(<ProjectPanel params={mockParams} />);
+            renderComponent();
             expect(screen.getByText(/Paragraph 1/i).tagName).toBe("P");
             expect(screen.getByText(/Paragraph 2/i).tagName).toBe("P");
             expect(screen.getByText(/Paragraph 3/i).tagName).toBe("P");
@@ -85,6 +95,47 @@ describe("UI/DOM Testing...", () => {
             renderComponent();
             const ele = screen.getByRole("list", { name: /Technologies Used/i });
             expect(ele.children.length).toBe(2);
+        });
+    });
+    describe("The list containing the device compatibilities...", () => {
+        test(`Should contain a 'Desktop Compatible' icon if the
+         'desktopCompatible' prop's value is true`, () => {
+            MaterialSymbolsIconRenderMock.mockImplementationOnce(() => {
+                return <div aria-label="device-compatibility-icon">Computer</div>;
+            })
+            render(<ProjectPanel params={{ ...mockParams, desktopCompatible: true }} />);
+            expect(screen.getByText(/Computer/i)).toBeInTheDocument();
+        });
+        test(`Should not contain a 'Desktop Compatible' icon if the
+         'desktopCompatible' prop's value is false`, () => {
+            render(<ProjectPanel params={{ ...mockParams, desktopCompatible: false }} />);
+            expect(screen.queryByText(/Computer/i)).not.toBeInTheDocument();
+        });
+        test(`Should contain a 'Mobile Compatible' icon if the
+         'mobileCompatible' prop's value is true`, () => {
+            MaterialSymbolsIconRenderMock.mockImplementationOnce(() => {
+                return <div aria-label="device-compatibility-icon">Mobile</div>;
+            })
+            render(<ProjectPanel params={{ ...mockParams, mobileCompatible: true }} />);
+            expect(screen.getByText(/Mobile/i)).toBeInTheDocument();
+        });
+        test(`Should not contain a 'Desktop Compatible' icon if the
+         'mobileCompatible' prop's value is false`, () => {
+            render(<ProjectPanel params={{ ...mockParams, mobileCompatible: false }} />);
+            expect(screen.queryByText(/Mobile/i)).not.toBeInTheDocument();
+        });
+        test(`Should contain a 'Tablet Compatible' icon if the
+         'tabletCompatible' prop's value is true`, () => {
+            MaterialSymbolsIconRenderMock.mockImplementationOnce(() => {
+                return <div aria-label="device-compatibility-icon">Tablet</div>;
+            })
+            render(<ProjectPanel params={{ ...mockParams, tabletCompatible: true }} />);
+            expect(screen.getByText(/Tablet/i)).toBeInTheDocument();
+        });
+        test(`Should not contain a 'Desktop Compatible' icon if the
+         'tabletCompatible' prop's value is false`, () => {
+            render(<ProjectPanel params={{ ...mockParams, tabletCompatible: false }} />);
+            expect(screen.queryByText(/Tablet/i)).not.toBeInTheDocument();
         });
     });
     describe(`The MaterialSymbolsAnchor component that links to the project's
