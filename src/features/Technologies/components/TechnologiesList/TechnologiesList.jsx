81945e962aa0d8b8cea489ca67bf099d6503187a
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-15 09:52:54 +0100

    Add fade-in animation to TechnologyPanels

--- src/features/Technologies/components/TechnologiesList/TechnologiesList.jsx
+++ src/features/Technologies/components/TechnologiesList/TechnologiesList.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TechnologiesList.module.css';
+
+import animateInViewport from '@/utils/animateInViewport';
 
 import {
     technologiesCategories,
@@ -13,12 +16,20 @@ const TechnologiesList = ({
     category,
     technologies,
 }) => {
+    const wrapperRef = useRef(null);
+
     const reducedTechnologiesArray = validateTechnologies(
         category, technologies, "TechnologiesList"
     );
 
+    useEffect(() => {
+        const eventListener = animateInViewport(wrapperRef.current);
+        window.addEventListener("scroll", eventListener);
+        return () => window.removeEventListener("scroll", eventListener);
+    }, [wrapperRef]);
+
     return (
-        <div className={styles["wrapper"]}>
+        <div className={styles["wrapper"]} ref={wrapperRef}>
         <div className={styles["container"]}>
             <h3 className={styles["category-title"]}>{getCategoryName(category)}</h3>
             <ul className={styles["technologies-panels-container"]}>
